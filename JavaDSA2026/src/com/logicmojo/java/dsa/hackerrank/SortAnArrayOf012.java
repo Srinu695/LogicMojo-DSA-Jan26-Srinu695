86949b55cddfee7fb0b515d686b67a00f9920410
Srinivas <srinivasrao695@gmail.com>
2026-02-13 07:51:32 +0530

    Adding dutch national flag practice problem

---
+++ JavaDSA2026/src/com/logicmojo/java/dsa/hackerrank/SortAnArrayOf012.java
@@ -0,0 +1,40 @@
+package com.logicmojo.java.dsa.hackerrank;
+
+import java.util.Arrays;
+
+/**
+ * Dutch National Flag Algorithm
+ */
+public class SortAnArrayOf012 {
+
+    public static void main(String[] args) {
+        int arr[] = {2,0,2,0,0,1,2,0,1,0,2};
+        System.out.println("Input:"+Arrays.toString(arr));
+        sort012Array(arr);
+    }
+
+    private static void sort012Array(int[] arr) {
+        int i=0,j=0,k=arr.length-1;
+        while(j<=k){
+
+            if(arr[j] == 1){
+                j++;
+            }
+
+            else if(arr[j] ==2 ){
+                int temp = arr[k];
+                arr[k] = arr[j];
+                arr[j] = temp;
+                k--;
+            }
+            else{
+               int temp = arr[j];
+               arr[j] = arr[i];
+               arr[i] = temp;
+               i++;j++;
+            }
+
+        }
+        System.out.println("output:"+Arrays.toString(arr));
+    }
+}
