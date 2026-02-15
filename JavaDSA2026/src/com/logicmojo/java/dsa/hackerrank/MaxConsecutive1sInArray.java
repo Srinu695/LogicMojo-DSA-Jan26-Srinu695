49bfe0ecf7b75020d48a72d2e57c2eb7f6ca8dcb
Srinivas <srinivasrao695@gmail.com>
2026-02-15 15:26:30 +0530

    Adding solved hacker rank problems

---
+++ JavaDSA2026/src/com/logicmojo/java/dsa/hackerrank/MaxConsecutive1sInArray.java
@@ -0,0 +1,37 @@
+package com.logicmojo.java.dsa.hackerrank;
+
+public class MaxConsecutive1sInArray {
+
+    public static void main(String[] args) {
+        int arr[] = {1,1,0,1,1,1,0,1,1};
+        System.out.println(findConsecutive1s(arr));
+        System.out.println("==============Sliding Window=================");
+        System.out.println(findConsecutive1sSlidingWindow(arr));
+    }
+
+    private static int findConsecutive1sSlidingWindow(int[] arr) {
+        int maxCount=0,j=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == 0){
+                j=i+1;
+            }
+            maxCount = Math.max(maxCount, i-j+1);
+        }
+        return maxCount;
+    }
+
+    private static int findConsecutive1s(int[] arr) {
+        int count=0, maxCount=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == 1){
+                count++;
+            }
+            else if(arr[i]==0){
+                maxCount = Math.max(count, maxCount);
+                count=0;
+            }
+        }
+        maxCount = Math.max(count, maxCount);
+        return maxCount;
+    }
+}
---
+++ JavaDSA2026/src/com/logicmojo/java/dsa/hackerrank/TwoSum.java
@@ -0,0 +1,46 @@
+package com.logicmojo.java.dsa.hackerrank;
+
+import java.util.Arrays;
+
+/**
+ *  This gives index of two elements which sums up to given target element
+ *  Index starts from zero
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int arr[] = {0,1,2,5,6,10};
+        int target = 16;
+
+        System.out.println(Arrays.toString(findIndexesWhereSumIsTarget(arr, target)));
+    }
+
+    private static int[] findIndexesWhereSumIsTarget(int[] arr, int target) {
+        int length = arr.length;
+        for(int i=0;i<length;i++){
+            int complement = target - arr[i];
+            int index = findComplementByBinarySearch(complement, arr);
+            if(index>=0){
+                return new int[]{i,index};
+            }
+        }
+        return arr;
+    }
+
+    private static int findComplementByBinarySearch(int complement, int[] arr) {
+        int i=0,j=arr.length-1;
+        while(i<=j){
+            int mid = i+ (j-i)/2;
+            if(arr[mid] == complement){
+                return mid;
+            }
+            else if(arr[mid]> complement){
+                j=mid-1;
+            }
+            else{
+                i= mid+1;
+            }
+        }
+        return -1;
+    }
+}
